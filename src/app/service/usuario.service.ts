9201d8463fdd24cedef9dc83cae5c53273d3d35e
mgaangel10 <mg.aangel10@gmail.com>
2025-04-06 01:34:27 +0200

    quitando palabras

--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -22,7 +22,7 @@ export class UsuarioService {
 
   constructor(private http:HttpClient) { }
   //url de backend
-  url='https://celeraai-backend.onrender.com';
+  url='http://localhost:9000';
 
   LoginResponseAdministrador(email: string, password: string): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${this.url}/auth/login/user`,
